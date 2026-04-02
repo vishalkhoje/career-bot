b08277402cd429ed31108f72150d25c4bd197efa
Vishal Khoje <vishalkhoje@gmail.com>
2026-04-02 16:51:01 +0530

    linkedIn profil and summury is updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-#First and foremost, deploy this for yourself! It's a real, valuable tool - the future resume..<br/>
+#It's a real, valuable tool - the future resume..<br/>
 # • Next, improve the resources - add better context about yourself. If you know RAG, then add a knowledge base about you.<br/>
 # • Add in more tools! You could have a SQL database with common Q&A that the LLM could read and write from?<br/>
 # • Bring in the Evaluator from the last lab, and add other Agentic patterns.
